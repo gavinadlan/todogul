6453db8eda199e6d8b0bcd90db1dc81803821700
gavin_adlan <117385972+gavinadlan@users.noreply.github.com>
2024-12-20 16:10:35 +0700

    Initial Commit

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'widgets/create_task_bottom_sheet.dart';
+import 'widgets/custom_drawer.dart';
+import 'widgets/custom_text_field.dart';
+import 'widgets/custom_dropdown.dart';
 
 class HomeScreen extends StatelessWidget {
   @override
@@ -14,19 +18,18 @@ class HomeScreen extends StatelessWidget {
             return IconButton(
               icon: Icon(Icons.menu, color: Colors.white),
               onPressed: () {
-                Scaffold.of(context).openDrawer();  // Open the Drawer when clicking the menu icon
+                Scaffold.of(context).openDrawer();
               },
             );
           },
         ),
         actions: [
-          // Logo Todogul diletakkan di pojok kanan
           Padding(
             padding: const EdgeInsets.only(right: 16),
             child: Image.asset(
-              'img/TODOGUL2.png',  // Logo Todogul tanpa lingkaran
-              width: 30,  // Ukuran logo diperkecil
-              height: 30,
+              'img/TODOGUL2.png',
+              width: 40,
+              height: 40,
             ),
           ),
         ],
@@ -34,40 +37,22 @@ class HomeScreen extends StatelessWidget {
       body: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          // Search bar diperkecil dan diposisikan lebih dekat ke atas
           Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10), // Diperpendek jarak vertikalnya
-            child: Container(
-              padding: EdgeInsets.all(6),
-              decoration: BoxDecoration(
-                color: Colors.transparent,
-                borderRadius: BorderRadius.circular(15),
-                border: Border.all(color: Colors.white, width: 1),
-              ),
-              child: TextField(
-                decoration: InputDecoration(
-                  hintText: 'Search tasks...',
-                  hintStyle: TextStyle(color: Colors.white, fontSize: 14),
-                  prefixIcon: Icon(Icons.search, color: Colors.white, size: 20),
-                  border: InputBorder.none,
-                ),
-              ),
-            ),
+            padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 5),
+            child: CustomTextField(hintText: 'Search tasks...'),
           ),
           SizedBox(height: 20),
-          // Gambar empty_tasks.png
           Center(
             child: Padding(
               padding: const EdgeInsets.only(top: 40),
               child: Image.asset(
-                'img/empaty_tasks.png',  // Pastikan gambar ada di folder assets
-                width: 200,  // Ukuran gambar dapat disesuaikan
+                'img/empaty_tasks.png',
+                width: 200,
                 height: 200,
               ),
             ),
           ),
           SizedBox(height: 20),
-          // Text di bawah gambar
           Center(
             child: Text(
               'What do you want to do today?',
@@ -99,276 +84,14 @@ class HomeScreen extends StatelessWidget {
               borderRadius: BorderRadius.vertical(top: Radius.circular(20)),
             ),
             builder: (context) {
-              return Padding(
-                padding: EdgeInsets.only(
-                  left: 16,
-                  right: 16,
-                  top: 16,
-                  bottom: MediaQuery.of(context).viewInsets.bottom + 16,
-                ),
-                child: Column(
-                  mainAxisSize: MainAxisSize.min,
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(
-                      'Create to-do',
-                      style: TextStyle(
-                        fontSize: 20,
-                        fontWeight: FontWeight.bold,
-                        color: Colors.white,
-                      ),
-                    ),
-                    SizedBox(height: 16),
-                    // Title Field
-                    TextField(
-                      decoration: InputDecoration(
-                        labelText: 'Title',
-                        labelStyle: TextStyle(color: Colors.white),
-                        enabledBorder: OutlineInputBorder(
-                          borderSide: BorderSide(color: Colors.white70),
-                        ),
-                        focusedBorder: OutlineInputBorder(
-                          borderSide: BorderSide(color: Colors.white),
-                        ),
-                      ),
-                      style: TextStyle(color: Colors.white),
-                    ),
-                    SizedBox(height: 16),
-                    // Description Field
-                    TextField(
-                      decoration: InputDecoration(
-                        labelText: 'Description',
-                        labelStyle: TextStyle(color: Colors.white),
-                        enabledBorder: OutlineInputBorder(
-                          borderSide: BorderSide(color: Colors.white70),
-                        ),
-                        focusedBorder: OutlineInputBorder(
-                          borderSide: BorderSide(color: Colors.white),
-                        ),
-                      ),
-                      style: TextStyle(color: Colors.white),
-                      maxLines: 3,
-                    ),
-                    SizedBox(height: 16),
-                    // Repeat Dropdown
-                    DropdownButtonFormField<String>(
-                      decoration: InputDecoration(
-                        labelText: 'Repeat',
-                        labelStyle: TextStyle(color: Colors.white),
-                        enabledBorder: OutlineInputBorder(
-                          borderSide: BorderSide(color: Colors.white70),
-                        ),
-                        focusedBorder: OutlineInputBorder(
-                          borderSide: BorderSide(color: Colors.white),
-                        ),
-                      ),
-                      dropdownColor: Colors.black,
-                      value: 'No Repeat',
-                      onChanged: (value) {},
-                      items: ['No Repeat', 'Daily', 'Weekly', 'Monthly']
-                          .map((repeatOption) {
-                        return DropdownMenuItem<String>(
-                          value: repeatOption,
-                          child: Text(
-                            repeatOption,
-                            style: TextStyle(color: Colors.white),
-                          ),
-                        );
-                      }).toList(),
-                    ),
-                    SizedBox(height: 16),
-                    // Days Selector
-                    DropdownButtonFormField<String>(
-                      decoration: InputDecoration(
-                        labelText: 'Day',
-                        labelStyle: TextStyle(color: Colors.white),
-                        enabledBorder: OutlineInputBorder(
-                          borderSide: BorderSide(color: Colors.white70),
-                        ),
-                        focusedBorder: OutlineInputBorder(
-                          borderSide: BorderSide(color: Colors.white),
-                        ),
-                      ),
-                      dropdownColor: Colors.black,
-                      value: 'Sunday',
-                      onChanged: (value) {},
-                      items: [
-                        'Sunday',
-                        'Monday',
-                        'Tuesday',
-                        'Wednesday',
-                        'Thursday',
-                        'Friday',
-                        'Saturday'
-                      ].map((day) {
-                        return DropdownMenuItem<String>(
-                          value: day,
-                          child: Text(
-                            day,
-                            style: TextStyle(color: Colors.white),
-                          ),
-                        );
-                      }).toList(),
-                    ),
-                    SizedBox(height: 16),
-                    // Date and Time Picker
-                    Row(
-                      children: [
-                        Expanded(
-                          child: ElevatedButton(
-                            onPressed: () async {
-                              await showDatePicker(
-                                context: context,
-                                initialDate: DateTime.now(),
-                                firstDate: DateTime(2000),
-                                lastDate: DateTime(2100),
-                                builder: (context, child) {
-                                  return Theme(
-                                    data: ThemeData.dark(),
-                                    child: child!,
-                                  );
-                                },
-                              );
-                            },
-                            child: Text('Select Date'),
-                          ),
-                        ),
-                        SizedBox(width: 16),
-                        Expanded(
-                          child: ElevatedButton(
-                            onPressed: () async {
-                              await showTimePicker(
-                                context: context,
-                                initialTime: TimeOfDay.now(),
-                                builder: (context, child) {
-                                  return Theme(
-                                    data: ThemeData.dark(),
-                                    child: child!,
-                                  );
-                                },
-                              );
-                            },
-                            child: Text('Select Time'),
-                          ),
-                        ),
-                      ],
-                    ),
-                    SizedBox(height: 16),
-                    // Completion Status Dropdown
-                    DropdownButtonFormField<String>(
-                      decoration: InputDecoration(
-                        labelText: 'Completion Status',
-                        labelStyle: TextStyle(color: Colors.white),
-                        enabledBorder: OutlineInputBorder(
-                          borderSide: BorderSide(color: Colors.white70),
-                        ),
-                        focusedBorder: OutlineInputBorder(
-                          borderSide: BorderSide(color: Colors.white),
-                        ),
-                      ),
-                      dropdownColor: Colors.black,
-                      value: 'Incomplete',
-                      onChanged: (value) {},
-                      items: ['Completed', 'Incomplete'].map((status) {
-                        return DropdownMenuItem<String>(
-                          value: status,
-                          child: Text(
-                            status,
-                            style: TextStyle(color: Colors.white),
-                          ),
-                        );
-                      }).toList(),
-                    ),
-                    SizedBox(height: 16),
-                    // Reminders Switch
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        Text(
-                          'Reminders',
-                          style: TextStyle(color: Colors.white),
-                        ),
-                        Switch(
-                          value: false,
-                          onChanged: (value) {},
-                          activeColor: Color(0xFF8687E7),
-                        ),
-                      ],
-                    ),
-                    SizedBox(height: 16),
-                    // Add Task Button
-                    ElevatedButton(
-                      onPressed: () {
-                        Navigator.pop(context);
-                      },
-                      style: ElevatedButton.styleFrom(
-                        backgroundColor: Color(0xFF8687E7),
-                      ),
-                      child: Center(
-                        child: Text('Add Task'),
-                      ),
-                    ),
-                  ],
-                ),
-              );
+              return CreateTaskBottomSheet();
             },
           );
         },
         backgroundColor: Color(0xFF8687E7),
         child: Icon(Icons.add, color: Colors.white),
       ),
-      drawer: Drawer(
-        backgroundColor: Colors.black,
-        child: ListView(
-          children: [
-            UserAccountsDrawerHeader(
-              accountName: Text(
-                'Agus Setiawan',
-                style: TextStyle(color: Colors.white),
-              ),
-              accountEmail: Text(
-                'a.stwn03@gmail.com',
-                style: TextStyle(color: Colors.white70),
-              ),
-              currentAccountPicture: CircleAvatar(
-                radius: 25,  // Menggunakan CircleAvatar untuk foto profil
-                backgroundImage: AssetImage('img/jerison.jpeg'),
-              ),
-              decoration: BoxDecoration(color: Colors.black),
-            ),
-            ListTile(
-              leading: Icon(Icons.edit, color: Colors.white),
-              title: Text(
-                'Edit Profile',
-                style: TextStyle(color: Colors.white),
-              ),
-              onTap: () {
-                // Handle profile editing here
-              },
-            ),
-            ListTile(
-              leading: Icon(Icons.settings, color: Colors.white),
-              title: Text(
-                'Settings',
-                style: TextStyle(color: Colors.white),
-              ),
-              onTap: () {
-                // Handle settings here
-              },
-            ),
-            ListTile(
-              leading: Icon(Icons.logout, color: Colors.white),
-              title: Text(
-                'Log Out',
-                style: TextStyle(color: Colors.white),
-              ),
-              onTap: () {
-                // Handle log out here
-              },
-            ),
-          ],
-        ),
-      ),
+      drawer: CustomDrawer(),
     );
   }
 }
